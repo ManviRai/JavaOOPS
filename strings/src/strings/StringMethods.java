52fdbecb1ebf426762888e3532122ade07404896
MANVITHA RAI A <120171970+ManviRai@users.noreply.github.com>
2024-07-29 15:42:27 +0530

    Add files via upload

---
+++ strings/src/strings/StringMethods.java
@@ -0,0 +1,14 @@
+package strings;
+
+public class StringMethods {
+  public static void main(String[] args) {
+	String name="Manvitha";
+	 name="Manvi";
+	String name1="a";
+	String name2="Manvi";
+	 System.out.println(name);
+	 System.out.println(name.toString());  
+	 System.out.println(name1.hashCode());   //ASCII Value
+	 System.out.println(name.equals(name2));
+}
+}
